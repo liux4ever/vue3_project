fe2f94c0895fc126286c9e69e4f761ad2cc91128
liux4ever <liux4ever@gmail.com>
2021-06-08 22:39:12 +0800

    globalHeader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="testData"></column-list>
   </div>
 </template>
@@ -8,6 +9,7 @@
 import { defineComponent } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -23,13 +25,32 @@ const testData: ColumnProps[] = [
     avatar:
       "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
   },
+  {
+    id: 3,
+    title: "test3的专栏",
+    description: "这是的test3专栏，有一段非常有意思的简介，可以更新一下欧",
+    // avatar:
+    //   "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
+  },
+  {
+    id: 4,
+    title: "test4的专栏",
+    description: "这是的test4专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar:
+      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
+  },
 ];
+const currentUser: UserProps = {
+  isLogin: false,
+  name: "lx",
+};
 export default defineComponent({
   name: "App",
-  components: { ColumnList },
+  components: { ColumnList, GlobalHeader },
   setup() {
     return {
       testData,
+      currentUser,
     };
   },
 });
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,23 +1,32 @@
 <template>
-  <ul>
-    <li v-for="item in list" :key="item.id">
-      <img :src="item.avatar" :alt="item.title" />
-      <h5>{{ item.title }}</h5>
-      <p>{{ item.description }}</p>
-      <a href="#">进入专栏</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img
+            :src="column.avatar"
+            :alt="column.title"
+            class="rounded-circle border border-light w-25 my-3"
+          />
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-left">{{ column.description }}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { PropType } from "vue";
+import { computed, PropType, defineComponent } from "vue";
 export interface ColumnProps {
   id: number;
   title: string;
-  avatar: string;
+  avatar?: string;
   description: string;
 }
-export default {
+export default defineComponent({
+  // defineComponent最重要的功能的是 在TS下给予组件 正确的类型判断
   name: "ColumnList",
   props: {
     list: {
@@ -25,7 +34,20 @@ export default {
       required: true,
     },
   },
-};
+  setup(props) {
+    const columnList = computed(() => {
+      return props.list.map((item) => {
+        if (!item.avatar) {
+          item.avatar = require("@/assets/logo.png");
+        }
+        return item;
+      });
+    });
+    return {
+      columnList,
+    };
+  },
+});
 </script>
 
 <style></style>
