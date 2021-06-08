59dfd39452b70309d8894f124b6c7827e0071e3e
liux4ever <liux4ever@gmail.com>
2021-06-08 19:41:20 +0800

    Column组件

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
-# vue3_project1
+# vue3_project
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li v-for="item in list" :key="item.id">
+      <img :src="item.avatar" :alt="item.title" />
+      <h5>{{ item.title }}</h5>
+      <p>{{ item.description }}</p>
+      <a href="#">进入专栏</a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { PropType } from "vue";
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+export default {
+  name: "ColumnList",
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
