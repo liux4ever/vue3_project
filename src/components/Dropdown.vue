c9c1ac4b3c69c5c30ec8f5ab37ad2363b7608daf
liux4ever <liux4ever@gmail.com>
2021-06-10 14:50:40 +0800

    hooks

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dropdown">
+  <div class="dropdown" ref="dropdownRef">
     <a
       href="#"
       class="btn btn-outline-light my-2 dropdown-toggle"
@@ -14,7 +14,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, onMounted, onUnmounted, ref, watch } from "vue";
+import useClickOutside from "../hooks/useClickOutside";
 export default defineComponent({
   name: "Dropdown",
   props: {
@@ -25,12 +26,20 @@ export default defineComponent({
   },
   setup() {
     const isOpen = ref(false);
+    const dropdownRef = ref<null | HTMLElement>(null);
     const toggleOpen = () => {
       isOpen.value = !isOpen.value;
     };
+    const isClickOutside = useClickOutside(dropdownRef);
+    watch(isClickOutside, () => {
+      if (isOpen.value && isClickOutside.value) {
+        isOpen.value = false;
+      }
+    });
     return {
       isOpen,
       toggleOpen,
+      dropdownRef,
     };
   },
 });
