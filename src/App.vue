e8e6cbcdecab0ca96a4d680cd091f433e1bd67e6
liux4ever <liux4ever@gmail.com>
2021-06-17 22:35:40 +0800

    Post组件

--- src/App.vue
+++ src/App.vue
@@ -38,36 +38,7 @@ import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
 import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
 import ValidateInput, { RulesProp } from "./components/ValidateInput.vue";
 import ValidateForm from "./components/ValidateForm.vue";
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: "test1的专栏",
-    description: "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
-    avatar:
-      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
-  },
-  {
-    id: 2,
-    title: "test2的专栏",
-    description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
-    avatar:
-      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
-  },
-  {
-    id: 3,
-    title: "test3的专栏",
-    description: "这是的test3专栏，有一段非常有意思的简介，可以更新一下欧",
-    // avatar:
-    //   "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
-  },
-  {
-    id: 4,
-    title: "test4的专栏",
-    description: "这是的test4专栏，有一段非常有意思的简介，可以更新一下欧",
-    avatar:
-      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
-  },
-];
+
 const currentUser: UserProps = {
   isLogin: false,
   name: "lx",
@@ -98,7 +69,6 @@ export default defineComponent({
       console.log(result);
     };
     return {
-      testData,
       currentUser,
       emailRules,
       emailValue,
--- src/views/ColumnDetail.vue
+++ src/views/ColumnDetail.vue
@@ -1,17 +1,39 @@
 <template>
-  <h1>
-    <pre>{{ route }}</pre>
-  </h1>
+  <div class="column-detail-page w-75 mx-auto">
+    <div
+      class="column-info row mb-4 border-bottom pb-4 align-items-center"
+      v-if="column"
+    >
+      <div class="col-3 text-center">
+        <img
+          :src="column.avatar && column.avatar"
+          :alt="column.title"
+          class="rounded-circle border w-100"
+        />
+      </div>
+      <div class="col-9">
+        <h4>{{ column.title }}</h4>
+        <p class="text-muted">{{ column.description }}</p>
+      </div>
+    </div>
+    <post-list :list="list"></post-list>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import { useRoute } from "vue-router";
+import { testData, testPosts } from "../testData";
+import PostList from "../components/PostList.vue";
 
 export default defineComponent({
+  components: { PostList },
   setup() {
     const route = useRoute();
-    return { route };
+    const currentId = route.params.id;
+    const column = testData.find((item) => item.id == currentId);
+    const list = testPosts.filter((post) => post.columnId == currentId);
+    return { column, list };
   },
 });
 </script>
