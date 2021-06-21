24cb56d660d83ed6eb08c55cb2f0b92c74956a88
liux4ever <liux4ever@gmail.com>
2021-06-21 19:07:00 +0800

    结构调整

--- src/App.vue
+++ src/App.vue
@@ -3,77 +3,31 @@
     <global-header :user="currentUser"></global-header>
     <!-- <column-list :list="testData"></column-list> -->
     <router-view></router-view>
-    <validate-form @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-        <validate-input
-          :rules="emailRules"
-          v-model="emailValue"
-          placeholder="请输入邮箱地址"
-          type="text"
-        >
-        </validate-input>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">密码</label>
-        <validate-input
-          v-model="passwordValue"
-          :rules="passwordRules"
-          placeholder="请输入密码"
-          type="password"
-        >
-        </validate-input>
-      </div>
-      <template #submit>
-        <span class="btn btn-danger">提交</span>
-      </template>
-    </validate-form>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">© 2021 知乎专栏</li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, ref } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
-import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
-import ValidateInput, { RulesProp } from "./components/ValidateInput.vue";
-import ValidateForm from "./components/ValidateForm.vue";
+import ColumnList from "./components/ColumnList.vue";
+import GlobalHeader from "./components/GlobalHeader.vue";
 import { useStore } from "vuex";
 
 export default defineComponent({
   name: "App",
-  components: { ColumnList, GlobalHeader, ValidateInput, ValidateForm },
+  components: { ColumnList, GlobalHeader },
   setup() {
     const store = useStore();
     const currentUser = computed(() => store.state.user);
-    const emailRules: RulesProp = [
-      {
-        type: "required",
-        message: "电子邮件不能为空",
-      },
-      {
-        type: "email",
-        message: "请输入正确的电子邮箱格式",
-      },
-    ];
-    const passwordRules: RulesProp = [
-      {
-        type: "required",
-        message: "密码不能为空",
-      },
-    ];
-    const emailValue = ref("");
-    const passwordValue = ref("");
-    const onFormSubmit = (result: boolean) => {
-      console.log(result);
-    };
     return {
       currentUser,
-      emailRules,
-      emailValue,
-      passwordRules,
-      passwordValue,
-      onFormSubmit,
     };
   },
 });
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,75 @@
 <template>
-  <div @click="routerJump">Login</div>
+  <div class="login-page mx-auto p-3 w-330">
+    <h5 class="my-4 text-center">登录到知乎</h5>
+    <validate-form @form-submit="onFormSubmit">
+      <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <validate-input
+          :rules="emailRules"
+          v-model="emailValue"
+          placeholder="请输入邮箱地址"
+          type="text"
+          ref="inputRef"
+        />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">密码</label>
+        <validate-input
+          type="password"
+          placeholder="请输入密码"
+          :rules="passwordRules"
+          v-model="passwordValue"
+        />
+      </div>
+      <template #submit>
+        <button type="submit" class="btn btn-primary btn-block btn-large">
+          登录
+        </button>
+      </template>
+    </validate-form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import { useRouter } from "vue-router";
+import ValidateInput, { RulesProp } from "../components/ValidateInput.vue";
+import ValidateForm from "../components/ValidateForm.vue";
+import { useStore } from "vuex";
 
 export default defineComponent({
+  components: { ValidateInput, ValidateForm },
   setup() {
     const router = useRouter();
-    const routerJump = () => {
-      router.push({ name: "column", params: { id: 1 } });
+    const store = useStore();
+    const emailRules: RulesProp = [
+      {
+        type: "required",
+        message: "电子邮件不能为空",
+      },
+      {
+        type: "email",
+        message: "请输入正确的电子邮箱格式",
+      },
+    ];
+    const passwordRules: RulesProp = [
+      {
+        type: "required",
+        message: "密码不能为空",
+      },
+    ];
+    const emailValue = ref("");
+    const passwordValue = ref("");
+    const onFormSubmit = (result: boolean) => {
+      console.log(result);
     };
     return {
-      routerJump,
+      emailValue,
+      passwordValue,
+      onFormSubmit,
     };
   },
 });
 </script>
 
-<style>
-</style>
+<style></style>
