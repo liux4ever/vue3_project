2034b544239ae98eba5c83566f4b1d502b6e2419
liux4ever <liux4ever@gmail.com>
2021-06-23 17:33:30 +0800

    posts route

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>posts</div>
+  <div>{{ route.params.id }}</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useRoute } from "vue-router";
 
-export default defineComponent({});
+export default defineComponent({
+  setup() {
+    const route = useRoute();
+    return { route };
+  },
+});
 </script>
 
 <style></style>
