d700205304465f3f1091d1ec0d517dd83717b919
liux4ever <liux4ever@gmail.com>
2021-06-30 21:18:43 +0800

    install axios

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
-import { createApp } from 'vue'
-import store from './store'
-import router from './router'
-import App from './App.vue'
+import { createApp } from "vue";
+import store from "./store";
+import axios from "axios";
+import router from "./router";
+import App from "./App.vue";
 
+axios.get("http://api.vikingship.xyz/api/columns").then((res) => {
+  console.log(res);
+});
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)
+  .mount("#app");
